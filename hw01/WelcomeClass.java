dffd46638df36184e2d546601c75e9fe7b3083a3
dtn216 <dtn216@lehigh.edu>
2015-09-01 04:09:22 +0000

    finished hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//Duc Ngo
+//Homework 01
+//WelcomeClass Java Program
+//
+// compile the program by using javac WelcomeClass.java
+// run program by using java WelcomeClass
+
+// first define the class
+public class WelcomeClass{
+    
+// add a main method
+public static void main(String[] args) {
+    
+    System.out.println("    ------------");
+    System.out.println("    |  WELCOME |");
+    System.out.println("    ------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-8-5-8-5-3-6-3-5-1-|");
+    System.out.println(" \\ / \\ / \\ / \\ / \\ /");
+    System.out.println("  v   v   v   v   v ");
+    System.out.println("I am a skydiver. My girlfriend does not approve of");
+    System.out.println("my hobby. I still do it nevertheless.");
+    
+}
+}
